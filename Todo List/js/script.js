76f52b9add2bc23f5ec2fdb72aa200cd2dcda4ff
Muhammad Sultan <125191342+Muhammad-Sultan-56@users.noreply.github.com>
2024-02-27 12:17:08 +0500

    Todo List Completed
    Todo List completed fully functional.

---
+++ Todo List/js/script.js
@@ -0,0 +1,81 @@
+const addUserBtn = document.getElementById("addUserBtn");
+const textBtn = addUserBtn.innerText;
+
+const addUserInput = document.getElementById("addUserInput");
+const records = document.getElementById("records");
+let userArray = [];
+let edit_id = null;
+
+let objStr = localStorage.getItem('name');
+
+if(objStr != null){
+    userArray = JSON.parse(objStr);
+}
+
+displayInfo();
+addUserBtn.onclick = () => {
+    const userName = addUserInput.value;
+
+    if(edit_id != null){
+        userArray.splice(edit_id , 1 , {'name' : userName} );
+        edit_id = null;
+    }
+    else{
+        userArray.push({'name' : userName});
+    }
+
+      saveInfo(userArray);
+    addUserInput.value = "";
+    addUserBtn.innerText = textBtn;
+}
+
+// function to save info
+function saveInfo(userArray) {
+
+    let userStr = JSON.stringify(userArray);
+    localStorage.setItem('name' , userStr)
+    displayInfo();
+
+}
+
+// function to display info
+function displayInfo() {
+
+    let statement = "";
+
+  
+    userArray.forEach((name , i) => {
+        statement += `
+        <tr>
+        <td>${i+1}</td>
+        <td>${name.name}</td>
+        <td>
+          <button class="btn btn-primary btn-sm me-2" onclick='editInfo(${i})'><i class="bi bi-pencil-square"></i></button>    
+          <button class="btn btn-danger btn-sm" onclick=deleteInfo(${i})><i class="bi bi-trash-fill"></i> </button>
+        </td>
+      </tr>`;
+    });
+
+    records.innerHTML = statement;
+
+}
+
+
+// function to edit info
+function editInfo(id) {
+   edit_id = id;
+   addUserInput.value = userArray[id].name;
+   addUserBtn.innerText = "Save Changes";
+
+}
+
+// function to delete info
+function deleteInfo(id) {
+    if(confirm("Are you Sure want to delete this Record")){
+        userArray.splice(id , 1 );
+        saveInfo(userArray);
+    }
+
+
+
+}
