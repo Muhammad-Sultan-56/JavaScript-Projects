720bbf6168cab7b9b5beac1a4d4d81cc4f05aaba
Muhammad Sultan <125191342+Muhammad-Sultan-56@users.noreply.github.com>
2024-04-08 11:54:17 +0500

    News Site
    UI design is created of News site using Bootstrap.

---
+++ New Articles using API/script.js
@@ -0,0 +1,4 @@
+
+
+const API_KEY = `5d6d261d9b7e4593b2d3d1c7e98e0584`;
+const API_URL = `https://newsapi.org/v2/everything?q=tesla&from=2024-03-08&sortBy=publishedAt&apiKey=5d6d261d9b7e4593b2d3d1c7e98e0584`;
